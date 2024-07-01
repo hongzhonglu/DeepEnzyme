df5b250eb114afd8ecf11fc5f6b16b92048586aa
Leo tongwang <102155568+dadadawt@users.noreply.github.com>
2024-07-01 22:21:47 +0800

    Update Fig2f.py

--- Code/Analysis/Fig2f.py
+++ Code/Analysis/Fig2f.py
@@ -1,30 +1,30 @@
 import matplotlib.pyplot as plt
 
-with open('../DeepEnzyme/Results/Output_results/random_1.txt') as input_1:
+with open('../../../DeepEnzyme/Results/Output_results/random_1.txt') as input_1:
     data = input_1.readlines()
     input_1.close()
 input_1_test = data[-1].split('\t')[-2]
 input_1_train = data[-1].split('\t')[5]
 
-with open('../DeepEnzyme/Results/Output_results/random_2.txt') as input_2:
+with open('../../../DeepEnzyme/Results/Output_results/random_2.txt') as input_2:
     data = input_2.readlines()
     input_2.close()
 input_2_test = data[-1].split('\t')[-2]
 input_2_train = data[-1].split('\t')[5]
 
-with open('../DeepEnzyme/Results/Output_results/random_3.txt') as input_3:
+with open('../../../DeepEnzyme/Results/Output_results/random_3.txt') as input_3:
     data = input_3.readlines()
     input_3.close()
 input_3_test = data[-1].split('\t')[-2]
 input_3_train = data[-1].split('\t')[5]
 
-with open('../DeepEnzyme/Results/Output_results/random_4.txt') as input_4:
+with open('../../../DeepEnzyme/Results/Output_results/random_4.txt') as input_4:
     data = input_4.readlines()
     input_4.close()
 input_4_test = data[-1].split('\t')[-2]
 input_4_train = data[-1].split('\t')[5]
 
-with open('../DeepEnzyme/Results/Output_results/random_5.txt') as input_5:
+with open('../../../DeepEnzyme/Results/Output_results/random_5.txt') as input_5:
     data = input_5.readlines()
     input_5.close()
 input_5_test = data[-1].split('\t')[-2]
@@ -50,4 +50,4 @@ plt.xticks(fontsize=12)
 plt.yticks(fontsize=12)
 
 plt.show()
-#plt.savefig("../../figure/Fig2f.pdf", dpi=600, bbox_inches='tight')
+#plt.savefig("../../../DeepEnzyme/Results/Figures/Fig2f.pdf", dpi=600, bbox_inches='tight')
