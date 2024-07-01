3a887200b413d8d14aa264de9a66d880e24241b5
Leo tongwang <102155568+dadadawt@users.noreply.github.com>
2024-07-01 22:20:19 +0800

    Update Fig2e.py

--- Code/Analysis/Fig2e.py
+++ Code/Analysis/Fig2e.py
@@ -1,16 +1,16 @@
 import matplotlib.pyplot as plt
 
-with open('../DeepEnzyme/Results/Output_results/training_onlysequences.txt') as training_onlysequences:
+with open('../../../DeepEnzyme/Results/Output_results/training_onlysequences.txt') as training_onlysequences:
     data = training_onlysequences.readlines()
     training_onlysequences.close()
 training_onlysequences = data[-1].split('\t')[-2]
 
-with open('../DeepEnzyme/Results/Output_results/training_onlystructure.txt') as training_onlystructure:
+with open('../../../DeepEnzyme/Results/Output_results/training_onlystructure.txt') as training_onlystructure:
     data = training_onlystructure.readlines()
     training_onlystructure.close()
 training_onlystructure = data[-1].split('\t')[-2]
 
-with open('../DeepEnzyme/Results/Output_results/dim64_lr001_E100_head4_drop3_0612_seed666.txt') as input:
+with open('../../../DeepEnzyme/Results/Output_results/dim64_lr001_E100_head4_drop3_0612_seed666.txt') as input:
     data = input.readlines()
     input.close()
 all_data = data[-1].split('\t')[-2]
@@ -26,4 +26,4 @@ plt.title('test set ${R^2}$')
 plt.ylabel('${R^2}$')
 
 plt.show()
-# plt.savefig("../../figure/Output_results.pdf", dpi=600, bbox_inches='tight')
+# plt.savefig("../../../DeepEnzyme/Results/Figures/Fig2e.pdf", dpi=600, bbox_inches='tight')
